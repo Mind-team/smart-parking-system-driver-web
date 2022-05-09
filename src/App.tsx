74e2c288ad09f86a3ad2342499c2fed1ccf3ecca
Ermolaev Ilya <Ermolaev.ID@yandex.ru>
2022-05-09 11:33:54 +0500

    routing & css

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { LoginContainer } from "./children/auth/login/pages/Login.container";
 import { HomePage } from "./children/cabinet/home/Home.page";
 import { CabinetLayout } from "./children/cabinet/Cabinet.layout";
@@ -15,6 +15,7 @@ function App() {
         <Route path="cabinet" element={<CabinetLayout />}>
           <Route path="home" element={<HomePage />} />
         </Route>
+        <Route path="*" element={<Navigate to="auth/login" replace />} />
       </Routes>
     </BrowserRouter>
   );
